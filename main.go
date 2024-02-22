a2783e633bbaf1ea131f6cabfc4a66d37a765eac
Zoey de Souza Pessanha <zoey.spessanha@zeetech.io>
2024-02-22 19:01:05 -0300

    feat: basic web server

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	port := flag.String("port", ":4000", "port that server will listen")
+
+	e := echo.New()
+
+	// middlewares
+	e.Use(middleware.RequestID())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+
+	e.GET("/", handleBasic)
+	e.Logger.Fatal(e.Start(*port))
+}
+
+func handleBasic(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, world!")
 }
